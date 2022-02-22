1152a464fd82fc6d667fc8e94b64168e24133992
Dmitry Sechkarenko <sarp69@icloud.com>
2022-02-22 17:32:10 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Dmitry Sechkarenko on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
